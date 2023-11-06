0f85cd01e2b890a731536689b6cfc86d630d3800
LukaLambrecht <Luka.Lambrecht@UGent.be>
2023-11-06 18:04:57 +0100

    bugfix in preprocessing

---
+++ preprocessing/preprocess.py
@@ -0,0 +1,107 @@
+################################################################
+# Preprocess histograms in a parquet file and store the result #
+################################################################
+
+# imports
+
+# external modules
+import sys
+import os
+import pandas as pd
+import numpy as np
+from pyarrow.parquet import ParquetFile
+import pyarrow as pa
+import argparse
+
+# ML4DQM modules
+sys.path.append(os.path.abspath('../../ML4DQMDC-PixelAE'))
+import jobsubmission.condortools as ct
+CMSSW = os.path.abspath('../../CMSSW_12_4_6')
+
+# local modules
+from preprocessor import PreProcessor
+
+
+if __name__=='__main__':
+
+  # read arguments
+  parser = argparse.ArgumentParser(description='Preprocess data')
+  parser.add_argument('-i', '--inputfile', required=True, type=os.path.abspath)
+  parser.add_argument('-o', '--outputfile', required=True, type=os.path.abspath)
+  parser.add_argument('-c', '--config', default=None, type=os.path.abspath)
+  parser.add_argument('-n', '--nentries', default=-1, type=int)
+  parser.add_argument('--omsfile', default=None, type=os.path.abspath)
+  parser.add_argument('--runmode', default='local', choices=['local','condor'])
+  args = parser.parse_args()
+
+  # print arguments
+  print('Running with following configuration:')
+  for arg in vars(args):
+    print('  - {}: {}'.format(arg,getattr(args,arg)))
+
+  # handle job submission if requested
+  if args.runmode=='condor':
+    cmd = 'python3 preprocess.py'
+    cmd += ' -i {}'.format(args.inputfile)
+    cmd += ' -o {}'.format(args.outputfile)
+    if args.config is not None: cmd += ' -c {}'.format(args.config)
+    if args.nentries > 0: cmd += ' -n {}'.format(args.nentries)
+    cmd += ' --runmode local'
+    ct.submitCommandAsCondorJob('cjob_preprocess', cmd,
+      cmssw_version=CMSSW, home='auto')
+    sys.exit()
+
+  # print starting tag (for job completion checking)
+  sys.stderr.write('###starting###\n')
+  sys.stderr.flush()
+
+  # define a PreProcessor
+  prep = PreProcessor(
+    crop=(slice(20,180), slice(19,179)),
+    time_average_radii={0: 2, 50:5},
+    rebin_target=(32,32),
+    omsjson=args.omsfile,
+    oms_normalization_attr='pileup'
+  )
+
+  # load the input file
+  if args.nentries <= 0:
+    df = pd.read_parquet(args.inputfile)
+  else:
+    pf = ParquetFile(args.inputfile)
+    batch = next(pf.iter_batches(batch_size = args.nentries))
+    df = pa.Table.from_batches([batch]).to_pandas()
+  nentries = len(df)
+
+  # extract and reshape the histograms
+  xbins = df['Xbins'][0]
+  ybins = df['Ybins'][0]
+  hists = np.array([df['histo'][i].reshape(xbins,ybins) for i in range(nentries)])
+  nhists = len(hists)
+  runs = np.array(df['fromrun'])
+  lumis = np.array(df['fromlumi'])
+
+  # do preprocessing
+  preprocessed_hists = prep.preprocess(hists, runs=runs, lumis=lumis)
+  xbins = preprocessed_hists.shape[1]
+  ybins = preprocessed_hists.shape[2]
+
+  # replace histograms in dataframe
+  preprocessed_hists = [preprocessed_hists[i,:,:].flatten() for i in range(nhists)]
+  df['histo'] = preprocessed_hists
+
+  # replace meta info to the dataframe
+  df['Xbins'] = (np.ones(len(lumis))*xbins).astype(int)
+  df['Ybins'] = (np.ones(len(lumis))*ybins).astype(int)
+
+  # write output file
+  df.to_parquet(args.outputfile)
+
+  # write preprocessor config file
+  if args.config is not None:
+    with open(args.config, 'w') as f:
+      f.write(prep.__str__())
+
+  # print finishing tag (for job completion checking)
+  sys.stderr.write('###done###\n')
+  sys.stderr.flush()
---
+++ preprocessing/preprocess_loop.py
@@ -0,0 +1,66 @@
+###################
+# Preprocess data #
+###################
+# Looper over preprocess.py
+
+# Note: crashes for some files with unclear error message,
+# most likely too little RAM for the unpacked dataframe,
+# try jobs with larger RAM request (but not urgent for now)
+
+# imports
+import sys
+import os
+from fnmatch import fnmatch
+import argparse
+sys.path.append(os.path.abspath('../../ML4DQMDC-PixelAE'))
+import jobsubmission.condortools as ct
+CMSSW = os.path.abspath('../../CMSSW_12_4_6')
+
+
+def get_oms_file(parquetfile):
+  if fnmatch(parquetfile, '*Run2023C-*-v1-*'): return 'omsdata/omsdata_Run2023C-v1.json'
+  if fnmatch(parquetfile, '*Run2023C-*-v2-*'): return 'omsdata/omsdata_Run2023C-v2.json'
+  if fnmatch(parquetfile, '*Run2023C-*-v3-*'): return 'omsdata/omsdata_Run2023C-v3.json'
+  if fnmatch(parquetfile, '*Run2023C-*-v4-*'): return 'omsdata/omsdata_Run2023C-v4.json'
+  if fnmatch(parquetfile, '*Run2023D-*-v1-*'): return 'omsdata/omsdata_Run2023D-v1.json'
+  if fnmatch(parquetfile, '*Run2023D-*-v2-*'): return 'omsdata/omsdata_Run2023D-v2.json'
+  if fnmatch(parquetfile, '*Run2023E-*-v1-*'): return 'omsdata/omsdata_Run2023E.json'
+  if fnmatch(parquetfile, '*Run2023F-*-v1-*'): return 'omsdata/omsdata_Run2023F.json'
+  raise Exception('No suitable OMS file found for input file {}'.format(parquetfile))
+
+
+if __name__=='__main__':
+
+  # read arguments
+  parser = argparse.ArgumentParser(description='Preprocess data')
+  parser.add_argument('-i', '--inputfiles', required=True, nargs='+')
+  parser.add_argument('--runmode', default='local', choices=['local','condor'])
+  args = parser.parse_args()
+
+  # print arguments
+  print('Running with following configuration:')
+  for arg in vars(args):
+    print('  - {}: {}'.format(arg,getattr(args,arg)))
+
+  # loop over input files
+  cmds = []
+  for inputfile in args.inputfiles:
+    outputfile = inputfile.replace('.parquet', '_preprocessed.parquet')
+    configfile = inputfile.replace('.parquet', '_prepconfig.txt')
+    omsfile = get_oms_file(inputfile)
+    cmd = 'python3 preprocess.py'
+    cmd += ' -i {}'.format(inputfile)
+    cmd += ' -o {}'.format(outputfile)
+    cmd += ' -c {}'.format(configfile)
+    cmd += ' --omsfile {}'.format(omsfile)
+    cmd += ' --runmode local'
+    cmds.append(cmd)
+
+  # run or submit commands
+  if args.runmode=='condor':
+    ct.submitCommandsAsCondorCluster('cjob_preprocess', cmds,
+      cmssw_version=CMSSW, home='auto')
+  else:
+    for cmd in cmds:
+      print(cmd)
+      os.system(cmd) 
