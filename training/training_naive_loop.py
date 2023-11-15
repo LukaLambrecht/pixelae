595d86bc29a5c8a211ea406bda0c68e24d6be65f
LukaLambrecht <Luka.Lambrecht@UGent.be>
2023-11-15 13:44:56 +0100

    add option for storing average response in training

---
+++ training/training_naive_loop.py
@@ -0,0 +1,77 @@
+#################################################
+# Run training for a set of monitoring elements #
+#################################################
+
+
+# imports
+# external modules
+import sys
+import os
+import argparse
+# ML4DQM modules
+sys.path.append(os.path.abspath('../../ML4DQMDC-PixelAE/jobsubmission'))
+import condortools as ct
+CMSSW = os.path.abspath('../../CMSSW_12_4_6')
+
+
+if __name__=='__main__':
+
+  # define monitoring elements
+  mes = ([
+    'PixelPhase1-Tracks-PXForward-clusterposition_xy_ontrack_PXDisk_+1',
+    'PixelPhase1-Tracks-PXForward-clusterposition_xy_ontrack_PXDisk_+2',
+    'PixelPhase1-Tracks-PXForward-clusterposition_xy_ontrack_PXDisk_+3',
+    'PixelPhase1-Tracks-PXForward-clusterposition_xy_ontrack_PXDisk_-1',
+    'PixelPhase1-Tracks-PXForward-clusterposition_xy_ontrack_PXDisk_-2',
+    'PixelPhase1-Tracks-PXForward-clusterposition_xy_ontrack_PXDisk_-3',
+  ])
+
+  # define eras
+  eras = ([
+    'Run2023C-PromptReco-v1',
+    'Run2023C-PromptReco-v2',
+    'Run2023C-PromptReco-v3',
+    'Run2023C-PromptReco-v4',
+    'Run2023D-PromptReco-v1',
+    'Run2023D-PromptReco-v2',
+    #'Run2023E-PromptReco-v1', # low occupancy
+    #'Run2023F-PromptReco-v1', # low occupancy
+  ])
+
+  # define input and output files
+  datadir = '/pnfs/iihe/cms/store/user/llambrec/dqmio/'
+  inputfiles = {}
+  outputfiles = {}
+  for me in mes:
+    mefiles = ['ZeroBias-{}-DQMIO-{}_preprocessed.parquet'.format(era, me) for era in eras]
+    mefiles = [os.path.join(datadir, f) for f in mefiles]
+    inputfiles[me] = mefiles
+    modelfile = 'model_20231110_{}.keras'.format(me)
+    outputfiles[me] = modelfile
+
+  # define other settings
+  settings = ({
+    'entries_threshold': 10000,
+    'skip_first_lumisections': 5,
+    'loss': 'mse',
+    'optimizer': 'adam',
+    'batch_size': 32,
+    'epochs': 30,
+    'validation_split': 0.1,
+    'store_average_response': 1
+  })
+
+  # make commands
+  cmds = []
+  for me in mes:
+    cmd = 'python3 training_naive.py'
+    cmd += ' -i {}'.format(' '.join(inputfiles[me]))
+    cmd += ' -o {}'.format(outputfiles[me])
+    cmd += ' --runmode local'
+    for arg, val in settings.items():
+      cmd += ' --{} {}'.format(arg, val)
+    cmds.append(cmd)
+
+  # submit jobs
+  ct.submitCommandsAsCondorCluster('cjob_training_naive', cmds,
+      cmssw_version=CMSSW, home='auto')
