243d6954b742c6ee7954998bafb1db323bce7659
LukaLambrecht <Luka.Lambrecht@UGent.be>
2023-11-22 16:00:55 +0100

    add ecal endcap model for testing

---
+++ models/ecalendcapmodel.py
@@ -0,0 +1,165 @@
+# Model definition of ECAL Endcap study
+# Reference: https://github.com/kyungminparkdrums/ML4ECALDQM/blob/master/resnetAE_EEocc.ipynb
+
+
+import random
+import copy
+import argparse
+import time
+import os, glob
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class ResBlock(nn.Module):
+    '''
+    For encoding, define the nominal resnet block
+    '''
+    def __init__(self, in_channels, out_channels):
+        super(ResBlock, self).__init__()
+        self.downsample = out_channels//in_channels
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=self.downsample, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=self.downsample)
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        if self.downsample > 1:
+            residual = self.shortcut(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResBlockTranspose(nn.Module):
+    '''
+    For decoding, define the transposed resnet block, aka "de-convolution"
+    '''
+    def __init__(self, in_channels, out_channels, out_shape=None):
+        super(ResBlockTranspose, self).__init__()
+        self.upsample = in_channels//out_channels  
+        self.convT1 = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=self.upsample, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.convT2 = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        self.shortcutT = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=1, stride=self.upsample)
+        self.out_shape = out_shape
+
+    def forward(self, x):
+        residual = x
+        
+        if self.out_shape is None:
+            out = self.convT1(x)
+        else:
+            out = self.convT1(x, output_size=(x.size()[0], x.size()[1], self.out_shape[0], self.out_shape[1]))
+        out = self.relu(out)
+        out = self.convT2(out)
+
+        if self.upsample > 1:
+            if self.out_shape is None:
+                residual = self.shortcutT(x)
+            else:
+                residual = self.shortcutT(x, output_size=(x.size()[0], x.size()[1], self.out_shape[0], self.out_shape[1]))
+            
+        out += residual
+        out = self.relu(out)
+
+        return out
+    
+class ResNetAE(nn.Module):
+    '''
+    Define the full ResNet autoencoder model
+    '''
+    def __init__(self, in_channels, nblocks, fmaps, debug=False):
+        super(ResNetAE, self).__init__()
+
+        self.fmaps = fmaps
+        self.nblocks = nblocks
+        self.in_channels = in_channels
+        self.debug = debug
+        
+        # Initialize encoding layers
+        self.econv0 = nn.Sequential(nn.Conv2d(in_channels, fmaps[0], kernel_size=5, stride=1, padding=(0,0)), nn.ReLU())
+        self.elayer1 = self.block_layers(self.nblocks, [fmaps[0],fmaps[0]], 'enc')
+        self.elayer2 = self.block_layers(1, [fmaps[0],fmaps[1]], 'enc')
+        self.elayer3 = self.block_layers(self.nblocks, [fmaps[1],fmaps[1]], 'enc')
+        
+        # Initialize decoding layers
+        self.fc = nn.Linear(self.fmaps[1], self.fmaps[1]*4*4)
+        self.dlayer3 = self.block_layers(self.nblocks, [fmaps[1],fmaps[1]], 'dec', out_shape=None)
+        self.dlayer2 = self.block_layers(1, [fmaps[1],fmaps[0]], 'dec', out_shape=(8, 8))
+        self.dlayer1 = self.block_layers(self.nblocks, [fmaps[0],fmaps[0]], 'dec', out_shape=None)
+        self.dconv0 = nn.ConvTranspose2d(fmaps[0], in_channels, kernel_size=5, stride=1, padding=(0,0))
+        self.dconv0_relu = nn.ReLU(inplace=True)
+
+    def block_layers(self, nblocks, fmaps, state, out_shape=None):
+        '''
+        Convenience function: append several resnet blocks in sequence
+        '''
+        layers = []
+        for _ in range(nblocks):
+            if state == 'enc':
+                layers.append(ResBlock(fmaps[0], fmaps[1]))
+            else:
+                layers.append(ResBlockTranspose(fmaps[0], fmaps[1], out_shape)) 
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        
+        # Encoding
+        if self.debug: print(x.size(), 'input')
+        if self.debug: print("Encode")
+        x = self.econv0(x)
+        if self.debug: print(x.size(), 'econv0')
+        x = F.max_pool2d(x, kernel_size=2)
+        if self.debug: print(x.size(), 'maxpool')
+
+        x = self.elayer1(x)
+        if self.debug: print(x.size(), 'elayer1')
+        x = self.elayer2(x)
+        if self.debug: print(x.size(), 'elayer2')
+        x = self.elayer3(x)
+        if self.debug: print(x.size(), 'elayer3')
+        
+        # Bottleneck comes from GlobalMaxPool
+        if self.debug: print("Bottleneck")
+        x = F.max_pool2d(x, kernel_size=x.size()[2:])
+        if self.debug: print(x.size(), 'GlobalMaxPool')
+        x = x.view(x.size()[0], -1)
+        if self.debug: print(x.size(), 'flatten')
+        
+        # Expand bottleneck
+        # Dimensions follow encoding steps in reverse, as much as possible
+        if self.debug: print("Expand bottleneck")
+        x = self.fc(x) # expand
+        if self.debug: print(x.size(), 'FC-upsample')
+        x = x.view(-1, self.fmaps[1], 4, 4)
+        if self.debug: print(x.size(), 'reshape')
+        
+        # Decoding
+        if self.debug: print("Decode")
+        x = self.dlayer3(x)
+        if self.debug: print(x.size(), 'dlayer3')
+        x = self.dlayer2(x)
+        if self.debug: print(x.size(), 'dlayer2')
+        x = self.dlayer1(x)
+        if self.debug: print(x.size(), 'dlayer1')
+        
+        x = F.interpolate(x, scale_factor=2)
+        if self.debug: print(x.size(), "interp")
+        x = self.dconv0(x, output_size=(x.size()[0], self.in_channels, 20, 20)) #needs to output 20x20
+        if self.debug: print(x.size(), 'dconv0')
+        x = self.dconv0_relu(x)
+            
+        return x
--- models/modeldefs.py
+++ models/modeldefs.py
@@ -6,25 +6,27 @@ from keras.layers import Flatten
 from keras.models import Model
 
 
-def residual_block_enc(x, filter_number, kernel_size, strides=1, **kwargs):
+def residual_block_enc(x, filter_number, kernel_size, strides=1, endactivation=None, **kwargs):
     y = Conv2D(filter_number, kernel_size, strides=strides, **kwargs)(x)
     y = ReLU()(y)
     y = Conv2D(filter_number, kernel_size, strides=1, **kwargs)(y)
-    x = Conv2D(filter_number, 1, strides=strides, **kwargs)(x)
+    if strides>1: x = Conv2D(filter_number, 1, strides=strides, **kwargs)(x)
     out = Add()([x, y])
-    #out = ReLU()(out)
+    if endactivation=='relu': out = ReLU()(out)
     return out
 
-def residual_block_dec(x, filter_number, kernel_size, strides=1, **kwargs):
+def residual_block_dec(x, filter_number, kernel_size, strides=1, endactivation=None, **kwargs):
     y = Conv2DTranspose(filter_number, kernel_size, strides=strides, **kwargs)(x)
     y = ReLU()(y)
     y = Conv2DTranspose(filter_number, kernel_size, strides=1, **kwargs)(y)
-    x = Conv2DTranspose(filter_number, 1, strides=strides, **kwargs)(x)
+    if strides>1: x = Conv2DTranspose(filter_number, 1, strides=strides, **kwargs)(x)
     out = Add()([x, y])
-    #out = ReLU()(out)
+    if endactivation=='relu': out = ReLU()(out)
     return out
 
+
 def model_dummy( input_shape ):
+    # for experimenting
     input_layer = Input(shape=input_shape)
     
     # encoder
@@ -44,54 +46,19 @@ def model_dummy( input_shape ):
     resnet_model.compile(loss="mse", optimizer="adam")
     return resnet_model
 
-def model_ecal_barrel( input_shape ):
+def model_mwe( input_shape ):
+    # minimal working example, november 15 2023
     input_layer = Input(shape=input_shape)
     
     # encoder
     resnet_layer = Conv2D(16, 3, strides=1, padding="same")(input_layer)
     resnet_layer = MaxPooling2D((2,2))(resnet_layer)
-    resnet_layer = residual_block_enc(resnet_layer, 16, 3, strides=1, padding="same")
+    resnet_layer = residual_block_enc(resnet_layer, 16, 3, strides=2, padding="same")
     resnet_layer = residual_block_enc(resnet_layer, 32, 3, strides=2, padding="same")
-    resnet_layer = residual_block_enc(resnet_layer, 32, 3, strides=1, padding="same")
-    resnet_layer = residual_block_enc(resnet_layer, 64, 3, strides=2, padding="same")
-    resnet_layer = residual_block_enc(resnet_layer, 64, 3, strides=1, padding="same")
-    shape = resnet_layer.shape
-    shape = (shape[1], shape[2])
-    resnet_layer = MaxPooling2D(shape)(resnet_layer)
     
     # decoder
-    resnet_layer = UpSampling2D(shape)(resnet_layer)
-    resnet_layer = residual_block_dec(resnet_layer, 64, 3, strides=1, padding="same")
-    resnet_layer = residual_block_dec(resnet_layer, 64, 3, strides=2, padding="same")
-    resnet_layer = residual_block_dec(resnet_layer, 32, 3, strides=1, padding="same")
     resnet_layer = residual_block_dec(resnet_layer, 32, 3, strides=2, padding="same")
-    resnet_layer = residual_block_dec(resnet_layer, 16, 3, strides=1, padding="same")
-    resnet_layer = UpSampling2D((2, 2))(resnet_layer)
-    output_layer = Conv2DTranspose(1, 3, strides=1, padding="same")(resnet_layer)
-    
-    # model
-    resnet_model = Model(inputs = [input_layer], outputs = [output_layer])
-    resnet_model.compile(loss="mse", optimizer="adam")
-    return resnet_model
-
-def model_ecal_endcap( input_shape ):
-    input_layer = Input(shape=input_shape)
-    
-    # encoder
-    resnet_layer = Conv2D(16, 3, strides=1, padding="same")(input_layer)
-    resnet_layer = MaxPooling2D((2,2))(resnet_layer)
-    resnet_layer = residual_block_enc(resnet_layer, 16, 3, strides=1, padding="same")
-    resnet_layer = residual_block_enc(resnet_layer, 32, 3, strides=2, padding="same")
-    resnet_layer = residual_block_enc(resnet_layer, 32, 3, strides=1, padding="same")
-    shape = resnet_layer.shape
-    shape = (shape[1], shape[2])
-    resnet_layer = MaxPooling2D(shape)(resnet_layer)
-    
-    # decoder
-    resnet_layer = UpSampling2D(shape)(resnet_layer)
-    resnet_layer = residual_block_dec(resnet_layer, 32, 3, strides=1, padding="same")
-    resnet_layer = residual_block_dec(resnet_layer, 32, 3, strides=2, padding="same")
-    resnet_layer = residual_block_dec(resnet_layer, 16, 3, strides=1, padding="same")
+    resnet_layer = residual_block_dec(resnet_layer, 16, 3, strides=2, padding="same")
     resnet_layer = UpSampling2D((2, 2))(resnet_layer)
     output_layer = Conv2DTranspose(1, 3, strides=1, padding="same")(resnet_layer)
     
