9a426f8dee627ab42275b08c2f8840235ce4a47e
LukaLambrecht <Luka.Lambrecht@UGent.be>
2025-05-17 00:47:17 +0200

    faster ls reading

--- tools/iotools.py
+++ tools/iotools.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 import pandas as pd
 import pyarrow as pa
-from pyarrow.parquet import ParquetFile
+from pyarrow.parquet import ParquetFile, ParquetDataset
 
 
 def read_parquet(path, verbose=False, 
@@ -62,41 +62,22 @@ def read_parquet(path, verbose=False,
         print(f'Read dataframe with {len(df)} rows and {len(df.columns)} columns.')
     return df
 
+
 def read_lumisections(path, run_numbers, ls_numbers,
                       verbose=False, columns=None,
                       run_column='run_number', ls_column='ls_number'):
-    # warning: very slow, use only for a few lumisections
+    """
+    Read specific lumisections from a file or list of files
+    Note: can be slow, do not use for many lumisections.
+    """
     
-    # convert path to list of paths to handle multiple input files
-    if isinstance(path, str): path = [path]
-        
-    # read run and lumisection numbers for each file
-    dfs = [read_parquet(p, columns=[run_column, ls_column]) for p in path]
-    
-    # loop over requested run and ls numbers
-    indices = []
+    # make filter
+    lsfilter = []
     for run_number, ls_number in zip(run_numbers, ls_numbers):
-        
-        # loop over dataframes and find out in which one (if any)
-        # the requested run and lumisection number are contained
-        index = []
-        for dfidx, df in enumerate(dfs):
-            rowidx = df.index[((df[run_column]==run_number) & (df[ls_column]==ls_number))].tolist()
-            for el in rowidx: index.append((dfidx, el))
-        if len(index)!=1:
-            msg = f'Found unexpected list of indices corresponding to run {run_number}, LS {ls_number}, skipping.'
-            print(msg)
-            continue
-        indices.append(index[0])
-            
-    # check if any lumisection could be found
-    if len(indices)==0:
-        msg = f'Found empty index list, returning None.'
-        print(msg)
-        return None
+        lsfilter.append( [(run_column, '=', run_number), (ls_column, '=', ls_number)] )
     
-    # read those specific rows
-    df = pd.concat([read_parquet(path[index[0]], verbose=False, columns=columns,
-                                 batch_size=1, first_batch=index[1], last_batch=index[1])
-                                 for index in indices])
+    # read dataframe
+    ds = ParquetDataset(path, filters=lsfilter)
+    df = ds.read().to_pandas()
+        
     return df
